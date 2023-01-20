861ca49443531240974c15f4bd2691d8dcce4546
Ubuntu <ubuntu@ip-172-31-16-96.ec2.internal>
2023-01-20 23:34:24 +0000

    feat: add google as name and get all ip address

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -6,18 +6,20 @@ import java.net.*;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        try{
+public class App {
+    public static void main(String[] args) {
+        try {
+            String name = "google.com";
             
-            String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
-            System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address.getHostAddress());
-        } catch (UnknownHostException e){
+            InetAddress address[] = InetAddress.getAllByName(name);
+            
+            System.out.println( "Name:      " + name);
+            
+            for(InetAddress addr: address) {
+                 System.out.println( "Address:   " + addr.getHostAddress());
+            }
+        } catch (UnknownHostException e) {
             System.out.println(e.getMessage());
-        }                
+        }
     }
 }
